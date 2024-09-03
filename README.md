7274905ee3763043017587a8eff444e17e239846
skxvtchy <jamesli784@gmail.com>
2024-09-03 00:43:54 -0400

    added transaction history

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # simple_blockchain
-Built with nodejs and typescript 
+
+- Built with node and typescript on top of
+- [FireShip Blockchain](https://youtu.be/qF7dkrce-mQ) video
+
+- Added transactiion history for wallet
+
+``` 
+// stores transaction
+public transactionHistory: Transaction[] = []; 
+
+// adds the transaction whem money sent
+this.transactionHistory.push(transaction);
+```
+
+
+`npm i` to to install node modules
+`npm start` to run
---
+++ index.ts
@@ -0,0 +1,145 @@
+import * as crypto from "crypto";
+
+// Transfer of funds between two wallets
+class Transaction {
+  constructor(
+    public amount: number,
+    public payer: string, // public key
+    public payee: string // public key
+  ) {}
+
+  toString() {
+    return JSON.stringify(this);
+  }
+}
+
+// Individual block on the chain
+class Block {
+  public nonce = Math.round(Math.random() * 999999999);
+
+  constructor(
+    public prevHash: string,
+    public transaction: Transaction,
+    public ts = Date.now()
+  ) {}
+
+  get hash() {
+    const str = JSON.stringify(this);
+    const hash = crypto.createHash("SHA256");
+    hash.update(str).end();
+    return hash.digest("hex");
+  }
+}
+
+// The blockchain
+class Chain {
+  // Singleton instance
+  public static instance = new Chain();
+
+  chain: Block[];
+
+  constructor() {
+    this.chain = [
+      // Genesis block
+      new Block("", new Transaction(100, "genesis", "satoshi")),
+    ];
+  }
+
+  // Most recent block
+  get lastBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  // Proof of work system
+  mine(nonce: number) {
+    let solution = 1;
+    console.log("⛏️  mining...");
+
+    while (true) {
+      const hash = crypto.createHash("MD5");
+      hash.update((nonce + solution).toString()).end();
+
+      const attempt = hash.digest("hex");
+
+      if (attempt.substr(0, 4) === "0000") {
+        console.log(`Solved: ${solution}`);
+        return solution;
+      }
+
+      solution += 1;
+    }
+  }
+
+  // Add a new block to the chain if valid signature & proof of work is complete
+  addBlock(
+    transaction: Transaction,
+    senderPublicKey: string,
+    signature: Buffer
+  ) {
+    const verify = crypto.createVerify("SHA256");
+    verify.update(transaction.toString());
+
+    const isValid = verify.verify(senderPublicKey, signature);
+
+    if (isValid) {
+      const newBlock = new Block(this.lastBlock.hash, transaction);
+      this.mine(newBlock.nonce);
+      this.chain.push(newBlock);
+    }
+  }
+}
+
+// Wallet gives a user a public/private keypair
+class Wallet {
+  public publicKey: string;
+  public privateKey: string;
+  public transactionHistory: Transaction[] = [];
+
+  constructor() {
+    const keypair = crypto.generateKeyPairSync("rsa", {
+      modulusLength: 2048,
+      publicKeyEncoding: { type: "spki", format: "pem" },
+      privateKeyEncoding: { type: "pkcs8", format: "pem" },
+    });
+
+    this.privateKey = keypair.privateKey;
+    this.publicKey = keypair.publicKey;
+  }
+
+  sendMoney(amount: number, payeePublicKey: string) {
+    const transaction = new Transaction(amount, this.publicKey, payeePublicKey);
+
+    this.transactionHistory.push(transaction);
+
+    const sign = crypto.createSign("SHA256");
+    sign.update(transaction.toString()).end();
+
+    const signature = sign.sign(this.privateKey);
+    Chain.instance.addBlock(transaction, this.publicKey, signature);
+  }
+
+  viewTransactionHistory() {
+    console.log(`Transaction History for Wallet: ${this.publicKey}`);
+    this.transactionHistory.forEach((transaction, index) => {
+      console.log(`Transaction ${index + 1}:`);
+      console.log(`  Amount: ${transaction.amount}`);
+      console.log(`  Payer: ${transaction.payer}`);
+      console.log(`  Payee: ${transaction.payee}`);
+    });
+  }
+}
+
+// Example usage
+
+const satoshi = new Wallet();
+const bob = new Wallet();
+const alice = new Wallet();
+
+satoshi.sendMoney(50, bob.publicKey);
+bob.sendMoney(23, alice.publicKey);
+alice.sendMoney(5, bob.publicKey);
+
+// transaction history
+satoshi.viewTransactionHistory();
+
+// console.log(Chain.instance);
